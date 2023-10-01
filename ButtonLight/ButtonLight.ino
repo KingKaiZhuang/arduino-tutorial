01611d03df87c4798d322cdb6e755132da69fa99
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-01 14:48:28 +0800

    update

--- ButtonLight/ButtonLight.ino
+++ ButtonLight/ButtonLight.ino
@@ -1,21 +1,54 @@
 boolean state = false;
-boolean buttonUp = true;
+boolean buttonDown = false;
+int buttonPressCount = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
-  pinMode(7,INPUT);
-  digitalWrite(7,HIGH);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  pinMode(7, INPUT_PULLUP); // 使用內部上拉電阻，按下為LOW，放開為HIGH
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(7) != HIGH && buttonUp == true){
-    state = !state;
-    digitalWrite(13,state);
-    buttonUp = false;
-  }else if(digitalRead(7) == HIGH && buttonUp != true){
-    buttonUp = true;
+  if (digitalRead(7) == LOW && !buttonDown) {
+    Serial.print("Counter = ");Serial.println(buttonPressCount);
+    buttonDown = true; // 表示按鈕被按下
+    buttonPressCount++;
+
+    // 根據按鈕按下次數執行不同的操作
+    switch (buttonPressCount) {
+      case 1:
+        // 第一次按下，LED亮一秒後滅掉
+        digitalWrite(13, HIGH);
+        delay(1000);
+        digitalWrite(13, LOW);
+        break;
+      case 2:
+        // 第二次按下，LED亮滅交替一秒，重複兩次
+        for (int i = 0; i < 2; i++) {
+          digitalWrite(13, HIGH);
+          delay(1000);
+          digitalWrite(13, LOW);
+          delay(1000);
+        }
+        break;
+      case 3:
+        // 第三次按下，LED亮滅交替一秒，重複三次
+        for (int i = 0; i < 3; i++) {
+          digitalWrite(13, HIGH);
+          delay(1000);
+          digitalWrite(13, LOW);
+          delay(1000);
+        }
+        break;
+      case 4:
+        // 第四次按下，LED全滅五秒
+        digitalWrite(13, LOW);
+        delay(5000);
+        buttonPressCount = 0;
+        break;
+    }
+  } else if (digitalRead(7) == HIGH && buttonDown) {
+    buttonDown = false; // 表示按鈕釋放
   }
 }
